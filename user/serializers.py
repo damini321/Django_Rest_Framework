184c9bb8a31556e31db094b031ea46f04e97eecd
damini321 <73902501+damini321@users.noreply.github.com>
2022-05-23 13:12:00 +0530

    Add files via upload

---
+++ user/serializers.py
@@ -0,0 +1,58 @@
+from allauth.account.adapter import get_adapter
+from rest_auth.registration.serializers import RegisterSerializer
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from . import models
+
+
+class CoreRegisterSerializer(RegisterSerializer):
+     class Meta:
+        model = models.User
+        fields = '__all__'
+
+     def get_cleaned_data(self):
+        return {
+            'username': self.validated_data.get('username', ''),
+            'email': self.validated_data.get('email', ''),
+            'first_name': self.validated_data.get('first_name', ''),
+            'password1': self.validated_data.get('password1', ''),
+            'password2': self.validated_data.get('password2', ''),
+            
+        }
+
+     def save(self, request):
+        adapter = get_adapter()
+        user = adapter.new_user(request)
+        self.cleaned_data = self.get_cleaned_data()
+        user.save()
+        adapter.save_user(request, user, self)
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.User
+        fields = '__all__'
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    model = models.User
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+
+class TokenSerializer(serializers.ModelSerializer):
+    user_type = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Token
+        fields = ('key', 'user', 'user_type')
+
+    def get_user_type(self, obj):
+        serializer_data = UserSerializer(
+            obj.user
+        ).data
+        
+
+        return {
+        }
